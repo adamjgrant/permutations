1174bc4afc25d6e7176fd7041a97074a6a7ee143
Adam Grant <adam_grant@apple.com>
2020-11-06 17:22:12 -0800

    Allowing for copying from clipboard

--- electron.js
+++ electron.js
@@ -2,15 +2,15 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 700,
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   win.loadFile('index.html')
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
