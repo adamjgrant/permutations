a64997b2df7256912ac776b2aa32f2145cc7567f
Adam Grant <adam_grant@apple.com>
2020-12-01 15:03:12 -0800

    Fixing spelling mistake

--- permy.link/script.js
+++ permy.link/script.js
@@ -143,7 +143,7 @@ const sanitize_gist_url = (url) => {
   const regex_1 = /^https\:\/\/gist\.githubusercontent\.com\//;
   const regex_1_1 = /^https\:\/\/github\.apple\.com\//;
   const regex_2 = /^https\:\/\/gist\.github\.com\//;
-  if (!url.match(regex_1) && !url.match(regex_1)) {
+  if (!url.match(regex_1) && !url.match(regex_1_1)) {
     if (!!url.match(regex_2)) {
       console.error("URL must start with gist.githubusercontent. Try opening the gist in a new tab and following the redirect first");
       setTimeout(() => {
