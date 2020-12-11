e5f76a243945bc7714c093530dd50a5accb09ee9
Adam Grant <adamgrant@Adams-MBP.lan>
2020-12-10 20:06:50 -0800

    Passing tests

--- permyscript.js
+++ permyscript.js
@@ -4,11 +4,33 @@ class PermyScript {
   }
 
   get compile() {
-    return JSON.parse(this.encapsulate.string);
+    return this.encapsulate
+        .split
+        .nest
+        .as_object
+  }
+
+  get split() {
+    const new_string = this.string.replaceAll("|", "\",\"");
+    return new PermyScript(new_string);
+  }
+
+  get nest() {
+    let new_string = this.string;
+    for (let x = 0; x < (this.string.match(/\(/g) || []).length; x++) {
+      new_string = `${new_string}]]`;
+    }
+    new_string = new_string.replaceAll("(", "\", [\"")
+    new_string = new_string.replaceAll(")", "\", [\"");
+    return new PermyScript(new_string);
   }
 
   get encapsulate() {
     return new PermyScript(`["${this.string}"]`);
+  }
+
+  get as_object() {
+    return JSON.parse(this.string);
   }
 }
 
