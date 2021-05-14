7cf0b7198fe0230c98368f78f390ea8d487dbaac
Adam Grant <adam_grant@apple.com>
2021-05-13 20:20:00 -0700

    Checkpoint

--- public/components/persistence/persistence_acts.js
+++ public/components/persistence/persistence_acts.js
@@ -26,7 +26,7 @@ m.persistence.acts({
     if (!address_id && !localstorage_id) {
       const new_id = _$.act.generate_a_new_document_id();
       _$.act.save_id_to_localstorage({ document_id: new_id });
-      _$.act.save_to_file({ data: m.editor.act.get_value() });
+      _$.act.save_to_file({ data: _$.act.load_code() });
       return new_id;
     }
 
@@ -41,31 +41,38 @@ m.persistence.acts({
     //   Use address's ID. Load from file, save ID and file contents to localstorage.
     // SCENARIO 4: ADDRESS HAS ID - LOCALSTORAGE HAS ID
     //   Same as SCENARIO 3.
+    if (address_id) {
+      _$.act.save_id_to_localstorage({ document_id: address_id });
+      _$.act.load_from_file().then(file_contents => {
+        m.editor.set_value({value: file_contents});
+        _$.act.save_code();
+      });
+    }
   },
 
   priv: {
     load_from_file(_$, args) {
-      var request = new XMLHttpRequest();
-      request.open('GET', `/document/${_$.act.get_document_id()}`, true);
+      return new Promise((resolve, reject) => {
+        var request = new XMLHttpRequest();
+        request.open('GET', `/document/${_$.act.get_document_id()}`, true);
 
-      request.onload = function() {
-        if (this.status >= 200 && this.status < 400) {
-          // Success!
-          return this.response;
-        } else {
-          // We reached our target server, but it returned an error
-          console.error("Could not load URL");
-          return
-        }
-      };
+        request.onload = function() {
+          if (this.status >= 200 && this.status < 400) {
+            // Success!
+            resolve(this.response);
+          } else {
+            // We reached our target server, but it returned an error
+            reject("Could not load URL");
+          }
+        };
 
-      request.onerror = function() {
-        // There was a connection error of some sort
-        console.error("Could not load URL");
-        return
-      };
+        request.onerror = function() {
+          // There was a connection error of some sort
+          reject("Could not load URL");
+        };
 
-      request.send();
+        request.send();
+      })
     },
 
     save_to_file(_$, args) {
