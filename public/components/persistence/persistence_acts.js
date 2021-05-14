df0ba206079116f79952debd06c32d7f40de629a
Adam Grant <adam_grant@apple.com>
2021-05-13 18:33:41 -0700

    Checkpoint

--- public/components/persistence/persistence_acts.js
+++ public/components/persistence/persistence_acts.js
@@ -4,13 +4,13 @@ m.persistence.acts({
     return localStorage.setObject("code", m.editor.act.get_value());
   },
 
-  load(_$, args) {
+  load_code(_$, args) {
     return localStorage.getObject("code");
   },
 
   set_initial_state(_$, args) {
-    if (_$.act.load().length) {
-      m.editor.act.set_value({ value: _$.act.load() });
+    if (_$.act.load_code().length) {
+      m.editor.act.set_value({ value: _$.act.load_code() });
     }
     else {
       m.editor.act.set_default_text();
@@ -18,10 +18,17 @@ m.persistence.acts({
   },
 
   get_document_id(_$, args) {
-    const address_url = _$.act.find_document_id_from_url();
+    const address_id      = _$.act.find_document_id_from_url();
+    const localstorage_id = _$.act.get_id_from_localstorage();
+
     // SCENARIO 1: ADDRESS NO ID - LOCALSTORAGE NO ID
     //   Create a new one, save it to localstorage, create a new file for writing.
-    const new_id = _$.act.generate_a_new_document_id();
+    if (address_id === undefined && localstorage_id === undefined) {
+      const new_id = _$.act.generate_a_new_document_id();
+      _$.act.save_id_to_localstorage({ document_id: new_id });
+      // TODO Continue
+    }
+
     // SCENARIO 2: ADDRESS NO ID - LOCALSTORAGE HAS ID
     //   Set the address to be the ID from localstorage and load from localstorage.
     // SCENARIO 3: ADDRESS HAS ID - LOCALSTORAGE NO ID
@@ -32,19 +39,50 @@ m.persistence.acts({
 
   priv: {
     load_from_file(_$, args) {
+      var request = new XMLHttpRequest();
+      request.open('GET', `/document/${_$.act.get_document_id()}`, true);
 
+      request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+          // Success!
+          return this.response;
+        } else {
+          // We reached our target server, but it returned an error
+          console.error("Could not load URL");
+          return
+        }
+      };
+
+      request.onerror = function() {
+        // There was a connection error of some sort
+        console.error("Could not load URL");
+        return
+      };
+
+      request.send();
     },
 
     save_to_file(_$, args) {
-
+      var request = new XMLHttpRequest();
+      request.open('POST', `/document/${_$.act.get_document_id()}`, true);
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.send(args.data);
     },
 
     find_document_id_from_url(_$, args) {
-
+      return undefined; // TODO
     },
 
     generate_a_new_document_id(_$, args) {
       return parseInt(`${Math.floor(Math.random() * 1000)}${Date.now()}`).toString(36);
+    },
+
+    get_id_from_localstorage(_$, args) {
+      return localStorage.getObject("document_id");
+    },
+
+    save_id_to_localstorage($, args) {
+      return localStorage.setObject("document_id", args.document_id);
     }
   }
 });
